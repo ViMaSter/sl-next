bcf9c3dca0f9e03c85ce8c9f1230d098c7f1f3d2
Vincent Mahnke <vincent@mahn.ke>
2024-11-01 09:34:15 +0100

    refactor: Replace with static files

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  assetPrefix: process.env.ASSET_PREFIX,
-  basePath: process.env.BASE_PATH,
-};
-
-export default nextConfig;
